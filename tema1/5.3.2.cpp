d42edc77f0297a8e739522c21f44cdf4736fdcfe
star2vec <ecaterina@stefanescu.eu>
2026-04-28 20:12:27 +0300

    Refactor linked list implementation in 5.3.2.cpp

--- tema1/5.3.2.cpp
+++ tema1/5.3.2.cpp
@@ -1,128 +1,42 @@
-#include<iostream>
+#include <iostream>
 
-int lista[1001];
-int next[1001];
-int n;
-
-void insertbeg(int k) {
-    next[n] = next[0];
-    next[0] = n;
-    lista[n] = k;
-    n++;
-}
-
-void delbeg(int k) {
-    next[0] = next[next[0]];
-}
-
-void insertend(int k) {
-    int i;
-
-    lista[n] = k;
-    next[n] = -1;
-
-    i=0;
-    while (next[i]!=-1) {
-        i = next[i];
-    }
-    next[i] = n;
-
-    n++;
-}
-
-void delend() {
-    int i;
-
-    i=0;
-    while (next[next[i]]!=-1) {
-        i = next[i];
-    }
-    next[i] = -1;
-}
-
-int findidx(int poz) {
-    int i, idx;
-    i=0;
-    idx = i;
-    while (i<poz && idx!=-1) {
-        idx = next[idx];
-        i++;
-    }
-    return lista[idx];
-}
-
-int findval(int val) {
-    int idx, i;
-    idx = 0;
-    i=0;
-    while (lista[idx]!=val && idx!=-1) {
-        idx = next[idx];
-        i++;
-    }
-    return i;
-}
-
-void insertpoz(int poz, int val) {
-    int i, idx;
-
-    i = 0;
-    idx = 0;
-    while (i<poz && idx!=-1) {
-        idx = next[idx];
-        i++;
-    }
-    if (idx!=-1) {
-        next[n] = next[idx];
-        next[idx] = n;
-        lista[n] = val;
-        n++;
-    }
-}
-
-void delpoz(int poz) {
-    int i, idx;
-
-    i = 0;
-    idx = 0;
-    while (i<poz && idx!=-1) {
-        idx = next[idx];
-        i++;
-    }
-
-    if (idx!=-1) {
-        next[idx] = next[next[idx]];
-    }
-}
-
-void coutlista() {
-    int idx;
-    idx = next[0];
-    while (idx != -1) {
-        std::cout << lista[idx] << " ";
-        idx = next[idx];
-    }
-    std::cout << '\n';
-}
+const int NMAX = 1001;
+int next[NMAX];
 
 int main() {
-    int idx, val;
-    n=1;
-    next[0] = -1;
+    int n, i, turtle, bunny, lung;
 
-    insertbeg(1);
-    insertbeg(2);
-    insertend(4);
-    insertend(8);
-    insertend(16);
+    std::cin >> n;
+    for (i=0; i<=n; i++) {
+        std::cin >> next[i];
+    }
 
-    coutlista();
+    turtle = bunny = 0;
+    do {
+        turtle = next[turtle];
+        if (next[bunny]!=-1) {
+            bunny = next[next[bunny]];
+        }
+    } while (turtle != bunny && bunny != -1 && next[bunny] != -1);
 
-    delbeg(0);
-    delend();
-    coutlista();
+    if (bunny == -1 || next[bunny] == -1) {
+            std::cout << "fara ciclu :P";
+    } else {
+        turtle = 0;
+        while (turtle != bunny) {
+            turtle = next[turtle];
+            bunny = next[bunny];
+        }
+        std::cout << "inceputul ciclului: " << bunny << '\n';
 
-    val = 8;
-    std::cout << findval(val);
+        lung = 1;
+        bunny = next[bunny];
+        while (turtle != bunny) {
+            bunny = next[bunny];
+            lung++;
+        }
+        std::cout << "lungime ciclu: " << lung << '\n';
+    }
 
     return 0;
 }
